5f1498360c46394eec53088e3abafedba3600091
Rory Sedgwick <rory.sedgwick@gmail.com>
2016-10-09 00:08:19 +0100

    update packages, json data, kanban swim lanes & progress updates

--- src/client/app/dashboard/dashboard.controller.js
+++ src/client/app/dashboard/dashboard.controller.js
@@ -5,12 +5,46 @@
     .module('app.dashboard')
     .controller('DashboardController', DashboardController);
 
-  DashboardController.$inject = ['$q', 'dataservice', 'logger'];
+  DashboardController.$inject = ['$q', 'dataservice', '$http', 'logger'];
   /* @ngInject */
-  function DashboardController($q, dataservice, logger) {
+  function DashboardController($q, dataservice, $http, logger) {
     var vm = this;
-
+console.log("loaded");
     vm.title = 'Dashboard';
+
+    $http.get('data/RSPO_Principle2.json')
+      .then(function(result) {
+
+        var data = result.data;
+
+        data = data.map(function(el) {
+          el.status = randomStatus();
+          return el;
+        });
+
+        console.log(data);
+
+        vm.data = data;
+
+      });
+
+
+      function randomStatus() {
+
+        var val = Math.random();
+
+        return val < 0.25 ? 'todo' : val < 0.5 ? 'in progress' : val < 0.75 ? 'in review' : 'complete';
+
+      }
+
+      vm.updateProgress = function(req) {
+        var index = vm.data.map(function(el) { return el.id }).indexOf(req.id);
+
+        var newStatus = req.status === 'todo' ? 'in progress' : req.status === 'in progress' ? 'in review' : 'complete';
+
+        vm.data[index].status = newStatus;
+      }
+
 
   }
 })();
---
+++ src/client/app/kanban-card/kanban-card.directive.js
@@ -0,0 +1,29 @@
+"use strict";
+
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .directive('kanbanCard', kanbanCard);
+
+    function kanbanCard () {
+
+        var directive = {
+            // controller: null,
+            // scope: {
+            //   'req': '='
+            // },
+            // templateUrl: 'kanban-card/kanban-card.html',
+            // link: function() {
+
+
+
+            // }
+        };
+
+        return directive;
+
+    }
+
+})();
