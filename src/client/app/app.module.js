1a0a6e206b147ad4e0b94d41a2bb3526bf42cbaa
Charlotte <charlotte.graves@arup.com>
2016-10-09 08:35:10 +0100

    starting parallel plots/fill gauge

--- src/client/app/app.module.js
+++ src/client/app/app.module.js
@@ -5,6 +5,7 @@
     'app.core',
     'app.dashboard',
     'app.teamView',
+    'app.parallelplot',
     'app.layout',
     'ngMaterial', 
     'ngMdIcons'
--- src/client/app/team_view/team_view.controller.js
+++ src/client/app/team_view/team_view.controller.js
@@ -11,10 +11,6 @@
 
         vm.title = 'Team View';
 
-
-
-        
-
         activate();
 
         ////////////////
@@ -29,6 +25,16 @@
                     el.status = randomStatus();
                     return el;
                     });
+
+                    var gauge1 = loadLiquidFillGauge("fillgauge1", 55);
+                    var config1 = liquidFillGaugeDefaultSettings();
+                    config1.circleColor = "#FF7777";
+                    config1.textColor = "#FF4444";
+                    config1.waveTextColor = "#FFAAAA";
+                    config1.waveColor = "#FFDDDD";
+                    config1.circleThickness = 0.2;
+                    config1.textVertPosition = 0.2;
+                    config1.waveAnimateTime = 1000;
 
                     console.log(data);
 
